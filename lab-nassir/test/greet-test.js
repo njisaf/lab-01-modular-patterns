03812a6d4c7162123c93e64f9527cb15a142906a
Nassir Isaf <njisaf@gmail.com>
2016-09-13 16:32:17 -0700

    Test for extra credit 2 is now working

--- lab-nassir/test/greet-test.js
+++ lab-nassir/test/greet-test.js
@@ -5,11 +5,18 @@ const assert = require('assert');
 
 describe('Greet test', function(){
   describe('#greet()', function(){
-    it('greet() should return \'Hello\' and a name; you can enter a name as a command line argument', function(){
+    it('greet() should return \'Hello\' plus an argument. Nassir is the test argument. Expected output: \'Hello Nassir\'', function(){
       assert.equal(greet('Nassir'), 'Hello Nassir');
     });
-    assert.throws(function(){
-      greet();
-    }, 'greet() expects an argument; should be a name.');
+    it('greet() should return \'Hello\' plus a command line input. Nassir is the test input. Expected output: \'Hello Nassir\'', function(){
+      var process = {};
+      process.argv = ['Empty', 'Empty', 'Nassir'];
+      assert.equal(greet(process.argv[2]), 'Hello ' + process.argv[2]);
+    });
+    it('greet() must have an argument. If it does not, it will throw an error.', function(){
+      assert.throws(function(){
+        greet();
+      }, 'greet() expects an argument; should be a name.');
+    });
   });
 });
