23517378e52841a198db295c80f976e52aef81b9
Nassir Isaf <njisaf@gmail.com>
2016-09-13 15:57:31 -0700

    Extra credit 1 done

--- lab-nassir/lib/greet.js
+++ lab-nassir/lib/greet.js
@@ -1,7 +1,14 @@
 'use strict';
 
-module.exports = function greet(name) {
-  console.log('arguments', arguments);
-  if (!name) throw new Error ('greet() expects an argument; should be a name');
-  return 'Hello ' + name;
+const greeting = module.exports = function(name) {
+  console.log('running');
+  if (process.argv[2]) {
+    console.log('Hello ' + process.argv[2]);
+  } else if (!name) {
+    throw new Error ('greet() expects an argument; should be a name');
+  } else {
+    return 'Hello ' + name;
+  }
 };
+
+greeting();
